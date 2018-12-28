740f9119a98f042727a2a34a01b98e38935618e5
cxksren <378537017@qq.com>
2018-12-28 16:58:00 +0800

    first test

---
+++ cxksrenly/src/main/java/cxksren/cxksrenly/App.java
@@ -0,0 +1,13 @@
+package cxksren.cxksrenly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
